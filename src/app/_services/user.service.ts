c0a5734b043e8111d896dad516b2c8d3fd1ff468
shisoni <shivangisoni1094@gmail.com>
2021-01-08 20:24:41 +0530

    commit

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from '@environments/environment';
-import { User} from 'user/_models';
+import { environment } from '../../environments/environment';
+import { User} from '../_models/user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { AuthenticationService } from '../_services';
-import { User } from '../_models';
+import { AuthenticationService } from './_services/authentication.service';
+import { User } from './_models/user';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
